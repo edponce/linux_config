314be8938fd1511af6439e7ecd96e2694db89da3
edponce <edponce00@hotmail.com>
2017-07-18 15:01:03 -0400

    improvements to xscreen.py and updated keybinds

--- bin/xscreen.py
+++ bin/xscreen.py
@@ -13,6 +13,8 @@ def parse_args():
                         dest='layout_id', help='layout identifier')
     parser.add_argument('-s', '--screen_id', type=int, default=0,
                         dest='screen_id', help='screen/monitor identifier')
+    parser.add_argument('-w', '--win_select', type=int, default=0,
+                        dest='win_select', help='window select identifier')
     args = parser.parse_args()
 
     # Validate
@@ -22,44 +24,48 @@ def parse_args():
     if args.screen_id < 0 or args.screen_id > 2:
         print('Error: invalid screen value.')
         exit(1)
+    if args.win_select < 0 or args.win_select > 1:
+        print('Error: invalid window select value.')
+        exit(1)
 
     return args
 
 
-def get_active_screen_dims(screen_pos = 0):
+def get_active_screen_dims(screen_id = 0):
     '''
     Get screen dimensions and offsets based on active window
-     0 -> screen with active window
-     1 -> lower-left screen
-     2 -> second screen
+     screen_id:
+      0 -> screen with active window
+      1 -> lower-left screen
+      2 -> second screen
     '''
-    screen_all_dims = []
-
-    # Get active window and dimensions
+    # Get active desktop
     desktop = int(subprocess.getoutput('xdotool get_desktop')) 
-    win_id = subprocess.getoutput('xdotool getactivewindow')
-    str_win_dims = subprocess.getoutput('. ~/bin/custom_utils; window_position ' + win_id + '; echo "${win_dims[@]} ${win_offs[@]}"').split()
-    win_dims = [int(x) for x in str_win_dims]
 
     # Get dimensions and offsets of lower-left screen
     str_screen_dims = subprocess.getoutput('. ~/bin/custom_utils; screen_position 1; echo "${screen_dims[@]} ${screen_offs[@]}"').split()
     screen_all_dims = [int(x) for x in str_screen_dims]
 
     # Use screen with active window
-    if screen_pos == 0:
+    if screen_id == 0:
+
         # Check for multiple screens
         num_monitors = int(subprocess.getoutput("xrandr --listactivemonitors | awk -F':' '/Monitors/ { print $2 }'"))
         if num_monitors > 1:
+            # Get active window and dimensions
+            win_id = subprocess.getoutput('xdotool getactivewindow')
+            str_win_dims = subprocess.getoutput('. ~/bin/custom_utils; window_position ' + win_id + '; echo "${win_dims[@]} ${win_offs[@]}"').split()
+            win_dims = [int(x) for x in str_win_dims]
             win_x = win_dims[0] + win_dims[2]
             win_y = win_dims[1] + win_dims[3]
 
-        # Check if active window is not in lower-left screen 
-        if win_x > screen_all_dims[0] or win_y > screen_all_dims[1]:
-            str_screen_dims = subprocess.getoutput('. ~/bin/custom_utils; screen_position 2; echo "${screen_dims[@]} ${screen_offs[@]}"').split()
-            screen_all_dims = [int(x) for x in str_screen_dims]
+            # Check if active window is not in lower-left screen 
+            if win_x > screen_all_dims[0] or win_y > screen_all_dims[1]:
+                str_screen_dims = subprocess.getoutput('. ~/bin/custom_utils; screen_position 2; echo "${screen_dims[@]} ${screen_offs[@]}"').split()
+                screen_all_dims = [int(x) for x in str_screen_dims]
 
     # Use second screen
-    elif screen_pos == 2:
+    elif screen_id == 2:
         # Check for multiple screens
         num_monitors = int(subprocess.getoutput("xrandr --listactivemonitors | awk -F':' '/Monitors/ { print $2 }'"))
         if num_monitors > 1:
@@ -73,29 +79,45 @@ def get_active_screen_dims(screen_pos = 0):
     return desktop, screen_dims, screen_offs
 
 
-def get_windows_list(desktop = 1):
+def get_windows_list(desktop = 1, win_select = 0, screen_dims = [], screen_offs = []):
     '''
     Get lists of visible window IDs and names
+     win_select:
+      0 -> visible windows in active screen/monitor 
+      1 -> visible windows in active desktop
     '''
     # Window queues
     win_ids = []
     win_names = []
     
+    # Check if screens are available 
+    if not screen_dims or not screen_offs:
+        print('Warning: incomplete screen/monitor properties.')
+        return win_ids, win_names
+
     # Get list of visible windows
-    win_list = subprocess.getoutput('wmctrl -l').split('\n')
+    win_list = subprocess.getoutput('wmctrl -l -G').split('\n')
     for wline in win_list:
         win_line = wline.split()
         win_id = win_line[0]
         win_desk = int(win_line[1])
+        win_off = [int(win_line[2]), int(win_line[3])]
+        win_dim = [int(win_line[4]), int(win_line[5])]
 
-        # Use windows in given desktop
-        if win_desk == desktop:
+        # Skip windows in other desktops
+        if win_desk != desktop:
+            continue
 
-            # Ignore specified windows
-            win_name = subprocess.getoutput("xprop -id " + win_id + " | awk -F'=' '/_OB_APP_NAME/ { print $2 }' | tr -d ' \"'").strip()
-            if not win_name in ['pcmanfm', 'xpad', 'galculator']:
-                win_ids.insert(0, win_id)
-                win_names.insert(0, win_name)
+        if win_select == 0: 
+            # Skip windows not in active screen/monitor
+            if win_off[0] >= screen_offs[0] + screen_dims[0] or win_off[0] + win_dim[0] <= screen_offs[0] or win_off[1] >= screen_offs[1] + screen_dims[1] or win_off[1] + win_dim[1] <= screen_offs[1]:
+                continue
+
+        # Ignore specified windows
+        win_name = subprocess.getoutput("xprop -id " + win_id + " | awk -F'=' '/_OB_APP_NAME/ { print $2 }' | tr -d ' \"'").strip()
+        if not win_name in ['pcmanfm', 'xpad', 'galculator']:
+            win_ids.insert(0, win_id)
+            win_names.insert(0, win_name)
 
     return win_ids, win_names
 
@@ -105,12 +127,18 @@ def create_layout(layout_id = 1, screen_dims = [], screen_offs = []):
     Create layout based on screen dimensions
     
     '''
+    # Window layout coordinates (integers), [[x0,y0],[x1,y1],...] 
+    win_dims = []
+    win_offs = []
+
+    # Check if screens are available 
+    if not screen_dims or not screen_offs:
+        print('Warning: incomplete screen/monitor properties.')
+        return win_dims, win_offs
+
     # Do not consider taskbar space
     screen_dims[1] -= 14 
 
-    # Compute window layouts coordinates (integers), [[x0,y0],[x1,y1],...] 
-    win_dims = []
-    win_offs = []
     if layout_id == 0:
         # h2v1 
         win_dims.insert(0, [int(screen_dims[0] / 2), int(screen_dims[1] / 2)])
@@ -243,12 +271,12 @@ def set_layout(win_ids = [], win_names = [], win_dims = [], win_offs = []):
     Arrange windows based on layout
     '''
     # Check if windows are available 
-    if not win_ids:
-        print('Warning: no windows were detected for processing.')
+    if not win_ids or not win_names or not win_dims or not win_offs:
+        print('Warning: incomplete windows properties for layout.')
         return 0
 
     # Create dictionary where keys -> win_names, values -> win_ids
-    win_dict = dict()
+    win_dict = {}
     num_wins = len(win_ids)
     for wi in range(0, num_wins):
         win_name = win_names[wi]
@@ -311,7 +339,7 @@ def set_layout(win_ids = [], win_names = [], win_dims = [], win_offs = []):
 if __name__ == '__main__':
     args = parse_args()
     desktop, screen_dims, screen_offs = get_active_screen_dims(args.screen_id)
-    win_ids, win_names = get_windows_list(desktop)
+    win_ids, win_names = get_windows_list(desktop, args.win_select, screen_dims, screen_offs)
     win_dims, win_offs = create_layout(args.layout_id, screen_dims, screen_offs)
     set_layout(win_ids, win_names, win_dims, win_offs)
 
